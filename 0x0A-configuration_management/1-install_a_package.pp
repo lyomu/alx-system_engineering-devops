912f122c787d2c620679e53797b8d2a24efe6ea1
lyomu <gmnyomu@gmail.com>
2022-12-13 15:35:59 -0800

    Corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# Installs a package
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem'
+# Creates a file
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
